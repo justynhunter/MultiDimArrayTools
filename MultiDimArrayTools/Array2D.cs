df19b6f30187ea069cfba5da061995142025d1da
Justyn Hunter <10092578+justynhunter@users.noreply.github.com>
2023-11-18 23:24:03 -0600

    add test project (#1)
    * add test project
    
    * remove obj folder
    
    * adds gitignore and applies it
    
    * adds some unit tests
    
    * select without index + aggregates
    
    * basic tests
    
    * aggregate unit tests
    
    ---------
    
    Co-authored-by: Justyn Hunter <justynhunter@gmail.com>

--- MultiDimArrayTools/Array2D.cs
+++ MultiDimArrayTools/Array2D.cs
@@ -1,5 +1,19 @@
 public static class Array2D
 {
+    public static U[,] Select<T, U>(this T[,] values, Func<T, U> fn)
+    {
+        var result = new U[values.GetLength(0), values.GetLength(1)];
+        for (int x = 0; x < values.GetLength(0); x++)
+        {
+            for (var y = 0; y < values.GetLength(1); y++)
+            {
+                result[x, y] = fn(values[x, y]);
+            }
+        }
+
+        return result;
+    }
+
     public static U[,] Select<T, U>(this T[,] values, Func<int, int, T, U> fn)
     {
         var result = new U[values.GetLength(0), values.GetLength(1)];
@@ -8,6 +22,34 @@ public static class Array2D
             for (var y = 0; y < values.GetLength(1); y++)
             {
                 result[x, y] = fn(x, y, values[x, y]);
+            }
+        }
+
+        return result;
+    }
+
+    public static U Aggregate<T, U>(this T[,] values, in U initalValue, Func<U, T, U> fn)
+    {
+        var result = initalValue;
+        for (int x = 0; x < values.GetLength(0); x++)
+        {
+            for (var y = 0; y < values.GetLength(1); y++)
+            {
+                result = fn(result, values[x, y]);
+            }
+        }
+
+        return result;
+    }
+
+    public static U Aggregate<T, U>(this T[,] values, in U initalValue, Func<U, int, int, T, U> fn)
+    {
+        var result = initalValue;
+        for (int x = 0; x < values.GetLength(0); x++)
+        {
+            for (var y = 0; y < values.GetLength(1); y++)
+            {
+                result = fn(result, x, y, values[x, y]);
             }
         }
 
